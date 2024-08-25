a893bd5f0d25180c3c372a1ad945c1ebe873d7b9
ChloeCrozier <chloecrozier@gmail.com>
2024-08-24 23:52:38 -0400

    creating xcode project

---
+++ contact_map/contact_map/contact_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  contact_mapApp.swift
+//  contact_map
+//
+//  Created by Chloe Crozier on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct contact_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
